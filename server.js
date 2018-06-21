28dfca97c3a27bf1cdd5580f43625d04e1f0c08b
Tommaso <cavazzatommaso00@gmail.com>
2018-06-21 11:31:30 +0200

    Update With params

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
+var pagine;
 
 app.get('/', function(req, res){
+    if(req.query.pag > 28 || req.query.pag < 1 ){
+        json = {error:"404",details:"Page not found (MIN = 1) (MAX = 28)"};
+        res.send(json);
+    }else{
+        pagine = 15*(req.query.pag-1);
 
-urlSito = 'http://www.iis-silva-ricci.gov.it/documenti/cat_view/1-circolari.html';
+urlSito = 'http://www.iis-silva-ricci.gov.it/documenti/cat_view/1-circolari.html'+'?start='+pagine;
 
 request(urlSito, function(error, response, html){
     if(!error){
@@ -33,6 +39,7 @@ fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
 res.send(json)
 
     }) ;
+    }
 })
 
 app.listen(process.env.PORT || 3000, function(){
